3f6f4fba426e93bffce3a9c61802dcc1dc6da232
D-Wolter <wolter.daniel@gmail.com>
2023-06-17 09:37:34 -0300

    commit inicial.

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,6 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_1 = Ingredient('queijo mussarela')
+
+    assert ingredient_1.name == 'queijo mussarela'
