a54a050f4728b5013fbf61e177e91194748868e7
D-Wolter <wolter.daniel@gmail.com>
2023-06-17 14:44:19 -0300

    feat: Req3 criada função para mapeamento pratos na classe src/services/menu_data.py

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,31 @@
+from models.ingredient import Ingredient
+from models.dish import Dish
+import csv
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        with open(source_path) as file:
+            self.ingredients = set()
+            all_dishes = csv.DictReader(file)
+            recipes = {}
+
+            for recipe in all_dishes:
+                ingredient_key = recipe["ingredient"]
+                ingredient_instance = Ingredient(ingredient_key)
+                self.ingredients.add(ingredient_instance)
+
+                dish_name = recipe["dish"]
+                dish_price = float(recipe["price"])
+                
+                if dish_name not in recipes:
+                    dish_instance = Dish(dish_name, dish_price)
+                    recipes[dish_name] = dish_instance
+
+                ingredient_quantity = int(recipe["recipe_amount"])
+                recipes[dish_name].add_ingredient_dependency(
+                    ingredient_instance, ingredient_quantity
+                )
+
+        self.dishes = set(recipes.values())
